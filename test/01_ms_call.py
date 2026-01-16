504ad81dda36b71eb525edd5a6fa07effde7e7ae
ameerusman <ameer.uak@gmail.com>
2026-01-16 11:54:14 +0500

    Testing function to test function calls

--- test/01_ms_call.py
+++ test/01_ms_call.py
@@ -9,6 +9,10 @@ by logging in and retrieving an asset.
 
 from ms_excel_win import ms_excel_wrappers
 import mainsequence.client as msc
+import datetime
+import ms_excel_win.auth as msauth
+
+msauth._load_tokens()
 
 print("Test Script")
 ms_excel_wrappers.login_dialog()
@@ -18,3 +22,14 @@ print("----------------")
 print(ms_excel_wrappers.get_asset("BBG000BS7KS3"))
 print("----------------")
 print(ms_excel_wrappers.get_asset("BBG000BY29C7"))
+print("----------------")
+
+'''
+curve_table_id	discount_curves
+date_start	29-Jun-25
+date_end	26-Dec-25
+'''
+
+start_date = datetime.datetime(2025, 6, 29)
+end_date = datetime.datetime(2025, 12, 26)
+print(ms_excel_wrappers.get_data_between_dates_from_node_identifier("discount_curves",start_date,end_date))
